e7a24a54735b154b77a9e5b92677682ba04ee709
ThePairry <60050157@kmitl.ac.th>
2018-10-26 19:06:11 +0700

    sendd

--- InstructionsMethod.java
+++ InstructionsMethod.java
@@ -2,19 +2,25 @@ import java.util.Scanner;
 class InstructionsMethod
 {
   static Scanner sc = new Scanner(System.in);
+  
   public static void getName()
   {
     String n = "ShowString."; 
     for(int i=1;i<=3;i++)
     System.out.println(n);
   }
+
+  public static void getInt()
+  {
+    System.out.print("Enter Integer =>");
+    int num = sc.nextInt();
+    System.out.println("Integer is "+num);
+  }
   
   public static void main(String[] args)
   { 
     getName();
     System.out.println();
-    System.out.print("Enter Integer =>");
-    int num = sc.nextInt();
-    System.out.println("Integer is "+num);
+    getInt();
   }
 }
